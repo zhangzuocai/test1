225313d4d0cd5ec002969e560597c5c34a1825d7
UID <UID@192.168.59.1>
2020-01-03 11:45:34 +0800

    123

--- _test/src/_test/Test1.java
+++ _test/src/_test/Test1.java
@@ -2,6 +2,6 @@ package _test;
 
 public class Test1 {
 	public static void main(String[] args) {
-		System.out.println("");
+		System.out.println("https://github.com/zhangzuocai/test1/blob/master/_test/src/_test/Test1.java");
 	}
 }
